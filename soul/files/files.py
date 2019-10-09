33b3d94e64c32a6d60d36031c9c7397f0979f741
Rik <rbose@cs.rochester.edu>
2019-10-09 04:37:00 -0400

    files

--- soul/files/files.py
+++ soul/files/files.py
@@ -2,9 +2,10 @@
 A bunch functions that would make my life easier
 """
 
-import os, re
+import os, re, soul
+import json as _json
 
-def ls(p=".", pattern=".*", depth=0):
+def ls_(p=".", pattern=".*", depth=0):
     # 1. If it is a file, return a list containing the file
     # 2. If it is a directory return all contents to depth 
     # 3. Filter results by pattern
@@ -22,3 +23,21 @@ def ls(p=".", pattern=".*", depth=0):
         return []
     return cands
 
+def ls(p=".", pattern=".*", depth=0):
+    return [os.path.join(p, x) for x in ls_(p, pattern, depth)]
+
+def json(file_):
+    if type(file_) is str:
+        with open(file_) as f:
+            return _json.load(f)
+    else:
+        return _json.load(file_)
+
+def dump_json(data, file_):
+    if type(file_) is str:
+        file_ = open(file_, 'w')
+    _json.dump(data, file_, indent=2)
+    file_.close()
+
+def jsons(string):
+    return _json.loads(string)
